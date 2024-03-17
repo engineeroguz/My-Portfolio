b0010f4dd543e11f90cd98ee20f8af414efd93fe
Oguzhan Bilce <bilceoguzhan@gmail.com>
2024-03-17 13:59:50 +0300

    first commit

---
+++ dist/js/icon.js
@@ -0,0 +1,10 @@
+// Footer icon tiklamak için
+let youtube = "https://www.youtube.com/";
+let myLinkedin =
+  "https://www.linkedin.com/in/o%C4%9Fuzhan-bil%C3%A7e-4683bb232/";
+let myGithub = "https://github.com/engineeroguz";
+let myInsta = "https://www.instagram.com/oguzhanbilce/#";
+let myX = "https://twitter.com/OguzhanBilce";
+function redirectToPage(pageUrl) {
+  window.location.href = pageUrl;
+}
